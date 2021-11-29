a04d190bc9c2ec11ef305d0821b2d5f7dbeeb511
jane-kim-279 <90606616+jane-kim-279@users.noreply.github.com>
2021-11-29 19:40:19 +0900

    Add files via upload

---
+++ images.js
@@ -0,0 +1,11 @@
+import UncheckedBox from '../assets/baseline_check_box_outline_blank_white_24.png';
+import CheckedBox from '../assets/baseline_check_box_white_24.png';
+import Delete from '../assets/baseline_delete_forever_white_24.png';
+import Edit from '../assets/baseline_edit_white_24.png';
+
+export const images = {
+    uncompleted: UncheckedBox,
+    completed: CheckedBox,
+    delete: Delete,
+    update: Edit,
+};
